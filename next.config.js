a91be34a09b26f8414a67836ecb440c55518a4cf
bulimba <rjfearn@gmail.com>
2026-05-13 16:15:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  images: { domains: ['oibigydthtoulttigtgy.supabase.co'] },
+  images: {
+    domains: ['oibigydthtoulttigtgy.supabase.co'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; object-src 'none';",
+          },
+        ],
+      },
+    ]
+  },
 }
+
+module.exports = nextConfig
